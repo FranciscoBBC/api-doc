138a42d818a4329551f5a92dcf83bc95ac1074c0
Francisco Castro <francisco.ruf@gmail.com>
2020-11-17 09:10:16 -0300

    responsive columns

--- components/MainWrapper.tsx
+++ components/MainWrapper.tsx
@@ -3,12 +3,16 @@ import Menu from '@components/Menu';
 
 const MainWrapper = () => {
   return (
-    <div className="flex mb-4">
-      <div className="w-1/6 bg-gray-200 h-screen">
+    <div className="flex flex-wrap">
+      <div className="w-full sm:w-full md:w-full lg:w-1/6 xl:w-1/6 bg-gray-200 md:h-auto sm:h-auto lg:h-screen xl:h-screen">
         <Menu />
       </div>
-      <div className="w-1/2 bg-white h-screen">seu</div>
-      <div className="w-1/3 bg-darkBlue h-screen">ze</div>
+      <div className="w-full sm:w-full md:w-full lg:w-1/2 xl:w-1/2 bg-white md:h-auto sm:h-auto lg:h-screen xl:h-screen">
+        seu
+      </div>
+      <div className="w-full sm:w-full md:w-full lg:w-1/3 xl:w-1/3 bg-darkBlue md:h-auto sm:h-auto lg:h-screen xl:h-screen">
+        ze
+      </div>
     </div>
   );
 };
